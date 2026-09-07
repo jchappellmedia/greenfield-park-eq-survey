80f63642ab154495560a5f6fd7ac93da8377cb1d
jchappellmedia <91850862+jchappellmedia@users.noreply.github.com>
2026-09-07 07:56:18 -0700

    Point survey at Apps Script Sheet sync endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Optional: after you deploy apps-script/Code.gs as a Google Apps Script web app,
 // paste the /exec URL here so responses also land in Google Sheets + GitHub Issues.
-window.SURVEY_ENDPOINT = "";
+window.SURVEY_ENDPOINT = "https://script.google.com/macros/s/AKfycbzALtLMty5fs4E8AiIBhRA7-czFJvtyrsNWzm3wJg161H5oRVshJ31xcuJRLwrgCQwm/exec";
 window.FORMSUBMIT_EMAIL = "jchappellmedia@gmail.com";
