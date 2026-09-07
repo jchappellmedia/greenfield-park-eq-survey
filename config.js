a64bc91e8fb335e43f0a37634f414eb97cd36dc7
jchappellmedia <91850862+jchappellmedia@users.noreply.github.com>
2026-09-06 17:36:20 -0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// Optional: after you deploy apps-script/Code.gs as a Google Apps Script web app,
+// paste the /exec URL here so responses also land in Google Sheets + GitHub Issues.
+window.SURVEY_ENDPOINT = "";
+window.FORMSUBMIT_EMAIL = "jchappellmedia@gmail.com";
